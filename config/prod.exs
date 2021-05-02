1b09ecb620e442107e8ecf10dbaf9ea80013a5d5
Zack Siri <zack@artellectual.com>
2021-05-02 14:24:39 +0700

    Add server true to prod

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,8 @@ use Mix.Config
 # before starting your production server.
 config :rdio, RdioWeb.Endpoint,
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  server: true
 
 # Do not print debug messages in production
 config :logger, level: :info
