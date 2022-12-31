54e1bd38560810193715b7ad3dd754411d050826
Zack Siri <me@zacksiri.dev>
2022-12-31 12:33:30 +0700

    Try adding react
    Signed-off-by: Zack Siri <me@zacksiri.dev>

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,6 +17,8 @@
 
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html"
+
+import React from 'react'
 // Establish Phoenix Socket and LiveView configuration.
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
