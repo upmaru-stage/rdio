2c1c2c4359f709a8b919221b81774f001b636f60
Zack Siri <me@zacksiri.dev>
2023-03-07 09:57:15 +0700

    update liveview

--- mix.exs
+++ mix.exs
@@ -46,7 +46,7 @@ defmodule Rdio.MixProject do
   defp deps do
     [
       {:bcrypt_elixir, "~> 3.0"},
-      {:phoenix, "~> 1.7.0-rc.0", override: true},
+      {:phoenix, "~> 1.7.0", override: true},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},
