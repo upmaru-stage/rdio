b86f2c81fbdd7abfa786685c55fd830b809350c3
Zack Siri <me@zacksiri.dev>
2023-10-26 19:01:20 +0700

    update migration task

--- lib/rdio/release/tasks.ex
+++ lib/rdio/release/tasks.ex
@@ -2,7 +2,7 @@ defmodule Rdio.Release.Tasks do
   @app :rdio
 
   def migrate do
-    Application.ensure_all_started(:ssl)
+    load_app()
 
     for repo <- repos() do
       {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
@@ -10,13 +10,16 @@ defmodule Rdio.Release.Tasks do
   end
 
   def rollback(repo, version) do
-    Application.ensure_all_started(:ssl)
-
+    load_app()
     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
   end
 
   defp repos do
-    Application.load(@app)
     Application.fetch_env!(@app, :ecto_repos)
+  end
+
+  defp load_app do
+    Application.load(@app)
+    Application.ensure_all_started(:ssl)
   end
 end
